7d4a8e753cb3f25b4dc6443be1b2e954fe3ada4d
Adriano Ferrari <adriano.ferrari@gmail.com>
2021-10-29 21:08:04 -0400

    Completed (w/o stretch goal)

---
+++ src/components/PerspectiveBackground/PerspectiveBackground.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './PerspectiveBackground.module.css';
+
+const PerspectiveBackground = () => {
+  return (
+    <div className={styles.background}></div>
+  );
+};
+
+export default PerspectiveBackground;
